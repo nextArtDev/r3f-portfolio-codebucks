2ef34f1dc615a7681e2f05e3b99ecae44d71ffbf
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-03-17 16:33:26 +0330

    threejs types fixing

--- components/models/Wizard.tsx
+++ components/models/Wizard.tsx
@@ -2,14 +2,17 @@
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
 
 export default function Wizard(props: any) {
   const { nodes, materials } = useGLTF('/models/coffeeMenu2.glb')
-  const modelRef = useRef()
+  const modelRef = useRef<THREE.Group>(null)
 
   useFrame((state) => {
-    modelRef.current?.position.y ==
-      -1.5 + Math.sin(state.clock.elapsedTime) * 0.15
+    if (modelRef.current) {
+      modelRef.current.position.y =
+        -1.5 + Math.sin(state.clock.elapsedTime) * 0.15
+    }
   })
   return (
     <group
@@ -22,7 +25,7 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Circle002.geometry}
+        geometry={(nodes.Circle002 as THREE.Mesh).geometry}
         material={materials['Material.001']}
         position={[0.012, 0.353, 0.004]}
         rotation={[-3.113, -0.663, -3.095]}
@@ -30,15 +33,15 @@ export default function Wizard(props: any) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Circle.geometry}
-          material={nodes.Circle.material}
+          geometry={(nodes.Circle as THREE.Mesh).geometry}
+          material={(nodes.Circle as THREE.Mesh).material}
           position={[0, -0.353, 0]}
         />
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Circle001.geometry}
-          material={nodes.Circle001.material}
+          geometry={(nodes.Circle001 as THREE.Mesh).geometry}
+          material={(nodes.Circle001 as THREE.Mesh).material}
           position={[0, -0.353, 0]}
           scale={[1, 1, 0.752]}
         />
@@ -46,7 +49,7 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Circle003.geometry}
+        geometry={(nodes.Circle003 as THREE.Mesh).geometry}
         material={materials['Material.004']}
         position={[3.195, 0.186, -0.037]}
         rotation={[Math.PI, -0.684, Math.PI]}
@@ -55,7 +58,7 @@ export default function Wizard(props: any) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Cube.geometry}
+          geometry={(nodes.Cube as THREE.Mesh).geometry}
           material={materials.Choc}
           position={[-0.288, 0.235, -0.395]}
           rotation={[-0.028, 0.144, 0.024]}
@@ -64,7 +67,7 @@ export default function Wizard(props: any) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Cube001.geometry}
+          geometry={(nodes.Cube001 as THREE.Mesh).geometry}
           material={materials.Choc}
           position={[-0.433, 0.229, 0.213]}
           rotation={[3.08, -1.116, 3.062]}
@@ -74,7 +77,7 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Cube002.geometry}
+        geometry={(nodes.Cube002 as THREE.Mesh).geometry}
         material={materials.Choc}
         position={[2.29, 0.621, -0.511]}
         rotation={[-0.123, 0.801, 0.108]}
@@ -83,7 +86,7 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Cube003.geometry}
+        geometry={(nodes.Cube003 as THREE.Mesh).geometry}
         material={materials.Choc}
         position={[2.388, 0.631, 0.355]}
         rotation={[-2.941, 1.138, 2.957]}
@@ -92,7 +95,7 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Cube004.geometry}
+        geometry={(nodes.Cube004 as THREE.Mesh).geometry}
         material={materials.Choc}
         position={[2.643, 0.604, -1.1]}
         rotation={[3.048, 0.701, 3.038]}
@@ -101,7 +104,7 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Cube005.geometry}
+        geometry={(nodes.Cube005 as THREE.Mesh).geometry}
         material={materials.Choc}
         position={[3.447, 0.645, -0.1]}
         rotation={[-3.13, -0.861, -3.129]}
@@ -110,7 +113,7 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Cube006.geometry}
+        geometry={(nodes.Cube006 as THREE.Mesh).geometry}
         material={materials.Choc}
         position={[3.153, 0.576, 0.907]}
         rotation={[-3.094, -0.363, 1.798]}
@@ -119,7 +122,7 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Circle004.geometry}
+        geometry={(nodes.Circle004 as THREE.Mesh).geometry}
         material={materials['Material.006']}
         position={[0.058, 0.711, 2.704]}
         rotation={[3.072, -0.392, 2.814]}
@@ -127,8 +130,8 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Circle005.geometry}
-        material={nodes.Circle005.material}
+        geometry={(nodes.Circle005 as THREE.Mesh).geometry}
+        material={(nodes.Circle005 as THREE.Mesh).material}
         position={[0.49, -0.147, 0.375]}
         rotation={[Math.PI, -0.684, Math.PI]}
         scale={0.607}
@@ -136,7 +139,7 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Circle006.geometry}
+        geometry={(nodes.Circle006 as THREE.Mesh).geometry}
         material={materials['Material.006']}
         position={[1.614, 0.29, 2.726]}
         rotation={[Math.PI, -0.684, Math.PI]}
@@ -144,7 +147,7 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Stick001.geometry}
+        geometry={(nodes.Stick001 as THREE.Mesh).geometry}
         material={materials.Cinnamon}
         position={[2.461, 0.29, -2.034]}
         rotation={[0, 1.316, 0]}
@@ -153,7 +156,7 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Stick002.geometry}
+        geometry={(nodes.Stick002 as THREE.Mesh).geometry}
         material={materials.Cinnamon}
         position={[-2.6, 0.62, -0.289]}
         rotation={[0.14, -1.005, -2.993]}
@@ -162,7 +165,7 @@ export default function Wizard(props: any) {
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Cube007.geometry}
+        geometry={(nodes.Cube007 as THREE.Mesh).geometry}
         material={materials.Choc}
         position={[3.112, 0.668, 0.136]}
         rotation={[0.703, -1.059, -2.383]}
