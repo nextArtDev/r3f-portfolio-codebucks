6803431e3c3726ecc96b07679674047b7bda4d86
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-03-22 23:06:19 +0330

    responsive about

--- app/(sub pages)/projects/page.tsx
+++ app/(sub pages)/projects/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 
       <ProjectList projects={projectsData} />
 
-      <div className="flex items-center justify-center fixed  top-16  lg:top-20 -translate-x-1/2 lg:translate-x-0 -z-10 left-1/2 lg:-left-24 h-screen">
+      <div className="flex items-center justify-center fixed  top-16  lg:top-20 -translate-x-1/2 lg:translate-x-0 -z-10 left-1/2 lg:-left-6 2xl:left-0 h-screen">
         <RenderModel>
           {/* <Staff /> */}
           <Cake />
